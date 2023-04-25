ad1fefd49276b9b278493b8e36017accf53b21d6
kesiaamrl <kesiacamaral@gmail.com>
2023-04-24 22:48:41 -0300

    aula3

--- src/App.java
+++ src/App.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Quantos funcionários deseja cadastrar? ");
+        int n=sc.nextInt();
+
+    Funcionario[] lista = new Funcionario[n]; //vetor
+        double soma=0;
+
+        for (int i=0; i<n; i++){
+            System.out.print("\033[H\033[2J");
+System.out.flush();
+        Funcionario func=new Funcionario();
+
+        System.out.println("Digite a matrícula do funcionário: ");
+        func.setMatricula(sc.next());
+
+        System.out.println("Digite o nome do funcionário: ");
+        func.setNome(sc.next());
+
+        System.out.println("Digite o salário do funcionário: ");
+        func.setSalario(sc.nextDouble());
+        soma += func.getSalario();
+        lista[i]=func;
+        }
+         System.out.print("\033[H\033[2J");
+System.out.flush();
+        for (Funcionario funcionario : lista) {
+            System.out.println(funcionario.getNome());
+            
+        }
+        System.out.println("A média salarial dos funcionários é: " + soma/n);
+        
     }
+    
+
 }
